a0d783fc300d18506d3e1391fbf7842914e23c5f
richo <richo@psych0tik.net>
2011-02-08 13:35:05 +1100

    Added some idea of what I'm hacking at

--- diff.py
+++ diff.py
@@ -4,6 +4,17 @@
 
 # I am writing this in such a way that other peeps can theoretically use it too.. :)
 
+
+# TODO:
+# - Group files by project
+# - 3 opts
+# -> Get diffs
+#  Show which projects have changes, and in which files
+# -> Marge svn>git
+#  Hose git with svn changeset
+# -> Merge git>svn
+#  Hose svn with git changeset
+
 import diff_file_maps
 import diff_user_maps
 
